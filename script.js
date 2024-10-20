96311a4179e12aa483a22501d95c93a7813cd7d8
nicoledeklerk <nicolenataliedeklerk@gmail.com>
2024-10-20 13:20:18 +0200

    Create conditional statement
    Created code block that tests if the input matches a pattern which produces a result of 'Valid Syntax' or 'Invalid Syntax' accordingly

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
     let correctPattern = /^pet_[a-zA-Z0-9]+$/;
+
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+        if (correctPattern.test(input)) {
+            result = 'Valid Syntax' + '\u{1F7E2}'; // The input is valid
+        } else {
+            result = 'Invalid Syntax' + '\u{1F534}'; // The input is invalid
+        }
 
             document.getElementById('result').innerText = result;
 }
