acbee1cda6d2378325849a1f4cd0e39afd7dc25f
nicoledeklerk <nicolenataliedeklerk@gmail.com>
2024-10-20 13:14:54 +0200

    Declare Pattern
    Declaring the pattern the input will be compared to for validation

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function validateSyntax() {
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
+    let correctPattern = /^pet_[a-zA-Z0-9]+$/;
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
             document.getElementById('result').innerText = result;
